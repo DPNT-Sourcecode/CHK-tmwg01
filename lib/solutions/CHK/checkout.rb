e8870eee132a30f105344e40a7e0565a89f1ec26
sbx_user1051 <sbx_user1051@169.254.63.253>
2024-01-29 18:28:26 +0000

    Mon Jan 29 18:28:26 UTC 2024

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -4,14 +4,17 @@ class Checkout
   VALID_SKUS = ['A', 'B', 'C', 'D'].freeze
 
   def checkout(skus)
-    skus_list = skus && skus.is_a?(String) ? skus.chars || []
+    skus_list = skus && skus.is_a?(String) ? skus.chars : []
     return -1 if invalid_skus?(skus)
+
+    0
   end
 
   private
 
   def invalid_skus?(skus)
-    skus.
+    false
   end
 
 end
+
