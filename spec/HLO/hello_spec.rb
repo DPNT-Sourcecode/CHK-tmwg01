e976ac9875fa77f931a2be33bc5722b2b1de0953
sbx_user1051 <sbx_user1051@169.254.63.253>
2024-01-29 18:07:12 +0000

    Mon Jan 29 18:07:12 UTC 2024

--- spec/HLO/hello_spec.rb
+++ spec/HLO/hello_spec.rb
@@ -3,7 +3,10 @@ require_solution 'HLO'
 describe Hello do
   subject { Hello.new }
 
+  let(:friend_name) { 'John' }
+
   it "says hello to the world" do
-    expect(subject.hello(nil)).to eq 'Hello, World!'
+    expect(subject.hello(friend_name)).to eq "Hello, #{friend_name}!"
   end
 end
+
